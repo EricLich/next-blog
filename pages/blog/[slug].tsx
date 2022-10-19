f6995436952280f0794f41f3eb85f93b54cfc74c
Eric Lich <eric.lichtensztein@aleia.io>
2022-10-18 22:51:36 -0300

    add static paths to blog posts and btn to go back

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,7 +1,63 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Router from "next/router";
 import React from "react";
+import client from "../../client";
+import { Post } from "../../src/utils/types";
 
-const BlogPost = () => {
-  return <div>BlogPost</div>;
+interface BlogPostProps {
+  post: Post;
+}
+
+const BlogPost: NextPage<BlogPostProps> = ({ post }) => {
+  return (
+    <>
+      <button onClick={() => Router.back()}>Go back!</button>
+      <h1>{post.title}</h1>
+      <span>By {post.author}</span>
+      <br />
+      {post?.body.map((parts) => {
+        let Tag = parts.children[0]._type;
+        return (
+          <>
+            <Tag key={parts.children[0].text}>{parts.children[0].text}</Tag>
+            <br />
+          </>
+        );
+      })}
+    </>
+  );
 };
 
 export default BlogPost;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  //@ts-ignore
+  const { slug } = context.params;
+  const post = await client.fetch(
+    `
+    *[_type == "post" && slug.current == $slug][0] {
+      'id': _id,
+      title,
+      'slug': slug.current,
+      'author': author->name,
+      body,
+    }
+  `,
+    { slug }
+  );
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await client.fetch(
+    `*[_type == "post" && defined(slug.current)][].slug.current`
+  );
+  return {
+    paths: paths.map((slug: any) => ({ params: { slug } })),
+    fallback: "blocking",
+  };
+};
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,16 @@
+import { ReactHTML } from "react";
+
 export type Post = {
   id: string;
   title: string;
   slug: string;
   author: string;
+  body: PostBody[];
 };
+
+export type PostBody = {
+  children: {
+    text: string;
+    _type: keyof ReactHTML;
+  }[]
+}
